6d94dd8e35b13b01a7bff40d37db16b373f276f9
Yauheni Akhotnikau <eao197@gmail.com>
2019-09-09 11:05:32 +0300

    Update.
    The correct implementation of macros in case of
    NOEXCEPT_CTCHECK_DISABLE.
    Debug output added to the example.

--- example/main.cpp
+++ example/main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 
+// Uncomment this to disable compile-time checks.
+//#define NOEXCEPT_CTCHECK_DISABLE
 #include <noexcept_ctcheck/pub.hpp>
 
 class safe_resource {
 public:
 	safe_resource() {}
 	~safe_resource() noexcept {
-		NOEXCEPT_CTCHECK_ENSURE_NOEXCEPT_STATEMENT(close());
+		NOEXCEPT_CTCHECK_ENSURE_NOEXCEPT_STATEMENT(release());
 	}
 
-	void acquire() {}
-	void use() {}
-	void close() noexcept {}
+	void acquire() { std::cout << "safe_resource::acquire()" << std::endl; }
+	void use() { std::cout << "safe_resource::use()" << std::endl; }
+	void release() noexcept { std::cout << "safe_resource::release()" << std::endl; }
 };
 
 class unsafe_resource {
@@ -19,14 +21,14 @@ public:
 	unsafe_resource() {}
 	~unsafe_resource() noexcept {
 		try {
-			NOEXCEPT_CTCHECK_ENSURE_NOT_NOEXCEPT_STATEMENT(close());
+			NOEXCEPT_CTCHECK_ENSURE_NOT_NOEXCEPT_STATEMENT(release());
 		}
 		catch(...) {}
 	}
 
-	void acquire() {}
-	void use() {}
-	void close() {}
+	void acquire() { std::cout << "unsafe_resource::acquire()" << std::endl; }
+	void use() { std::cout << "unsafe_resource::use()" << std::endl; }
+	void release() { std::cout << "unsafe_resource::release()" << std::endl; }
 };
 
 template< typename T >
